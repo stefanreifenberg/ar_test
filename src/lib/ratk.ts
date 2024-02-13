f437f7e6c92b75c5f55641cf31f0416d77439aec
Stefan Reifenberg <stefan.reifenberg@googlemail.com>
2024-02-13 09:14:36 +0100

    add handleMesh

--- src/lib/ratk.ts
+++ src/lib/ratk.ts
@@ -1,13 +1,61 @@
 import { getContext, setContext } from 'svelte'
 import { RealityAccelerator } from 'ratk'
 import { useThrelte, useTask } from '@threlte/core'
+import {
+	BoxGeometry,
+	BufferGeometry,
+	DirectionalLight,
+	HemisphereLight,
+	Line,
+	Mesh,
+	MeshBasicMaterial,
+	PerspectiveCamera,
+	Scene,
+	SphereGeometry,
+	Vector3,
+	WebGLRenderer,
+} from 'three';
+import { Text } from 'troika-three-text';
 
 const key = Symbol('ratk')
 
 export const createRatk = () => {
 	const { renderer } = useThrelte()
 	const ratk = new RealityAccelerator(renderer.xr)
-	useTask(() => ratk.update())
+		/**
+	 * Handles the addition of a new plane detected by RATK.
+	 */
+	function handlePlaneAdded(plane) {
+		const mesh = plane.planeMesh;
+		mesh.material = new MeshBasicMaterial({
+			wireframe: true,
+			color: Math.random() * 0xffffff,
+		});
+	}
+
+	/**
+	 * Handles the addition of a new mesh detected by RATK.
+	 */
+	function handleMeshAdded(mesh) {
+		const meshMesh = mesh.meshMesh;
+		meshMesh.material = new MeshBasicMaterial({
+			wireframe: true,
+			color: Math.random() * 0xffffff,
+		});
+		meshMesh.geometry.computeBoundingBox();
+		const semanticLabel = new Text();
+		meshMesh.add(semanticLabel);
+		semanticLabel.text = mesh.semanticLabel;
+		semanticLabel.anchorX = 'center';
+		semanticLabel.anchorY = 'bottom';
+		semanticLabel.fontSize = 0.1;
+		semanticLabel.color = 0x000000;
+		semanticLabel.sync();
+		semanticLabel.position.y = meshMesh.geometry.boundingBox.max.y;
+		mesh.userData.semanticLabelMesh = semanticLabel;
+	}
+
+	
 	setTimeout(() => {
 		// if (ratk.planes.size == 0) {
 		// 	console.log('initiateRoomCapture')
@@ -15,6 +63,8 @@ export const createRatk = () => {
 		// }
 		renderer.xr.getSession().initiateRoomCapture();
 	}, 5000);
+
+	useTask(() => ratk.update())
 	setContext(key, ratk)
 }
 
