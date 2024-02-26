d5230bfc89d7266c4465bea595d165231a486d9b
Stefan Reifenberg <stefan.reifenberg@googlemail.com>
2024-02-26 16:19:19 +0100

    test more anchor stuff

--- src/lib/components/Anchors.svelte
+++ src/lib/components/Anchors.svelte
@@ -12,32 +12,38 @@
 
     useTask((delta) => {
 
-    if (pendingAnchorsData) {
-        const isPersistent = true
-        const isRecovered = false
+        if (pendingAnchorsData) {
+            const isPersistent = true
+            let anchorPosition
+            let anchorQuaternion
+            
+            const matrix = pendingAnchorsData
+            const position = new THREE.Vector3()
+            const quaternion = new THREE.Quaternion()
+            const scale = new THREE.Vector3()
+            matrix.decompose(position, quaternion, scale)
+            anchorPosition = position
+            anchorQuaternion = quaternion
 
-        const anchorPosition = new THREE.Vector3(1, 2, 3);
-        const anchorQuaternion = new THREE.Quaternion(0, 0, 0, 1);
+            const geometry = new THREE.BoxGeometry(1, 1, 1);
+            const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 
-        const geometry = new THREE.BoxGeometry(1, 1, 1);
-        const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+            // create an anchor
+            console.log('creating anchor')
+            console.log("ratk",ratk)
+            console.log("ratk.anchors",ratk.anchors)
 
-        // create an anchor
-        console.log('creating anchor')
-        console.log("ratk",ratk)
-        console.log("ratk.anchors",ratk.anchors)
+            ratk.createAnchor(anchorPosition, anchorQuaternion, isPersistent)
+                .then((anchor /* RATK Anchor object extends Object3D */) => {
 
-        ratk.createAnchor(anchorPosition, anchorQuaternion, isPersistent)
-            .then((anchor /* RATK Anchor object extends Object3D */) => {
+                    console.log('anchor', anchor)
 
-                console.log('anchor', anchor)
-
-                // Attach a new THREE.Mesh to the anchor
-                anchor.add(new THREE.Mesh(geometry, material));
-                anchors = [...ratk.anchors]
-            });
-    }
-    pendingAnchorsData = null
+                    // Attach a new THREE.Mesh to the anchor
+                    anchor.add(new THREE.Mesh(geometry, material));
+                    anchors = [...ratk.anchors]
+                });
+        }
+        pendingAnchorsData = null
   })
     // check if the pendingAnchorsData is not null
    
