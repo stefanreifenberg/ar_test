384e07f6391b8dac0ba617b1d65cc465eccdc5dc
Stefan Reifenberg <stefan.reifenberg@googlemail.com>
2024-02-26 10:10:49 +0100

    try download button

---
+++ src/lib/components/Label.svelte
@@ -0,0 +1,25 @@
+<script>
+    import { Text } from '@threlte/extras'
+    import { useReflow } from '@threlte/flex'
+    import { forwardEventHandlers } from '@threlte/core'
+    export let text
+    export let color = '#000'
+    export let z = 0
+    
+    export let anchorX = '50%'
+    export let anchorY = '50%'
+    export let fontSize = 0.11
+    
+    const fontUrl = '/fonts/Barlow-Regular.ttf'
+    const reflow = useReflow()
+  </script>
+  <Text
+    font={fontUrl}
+    position.z={z}
+    {text}
+    {anchorX}
+    {anchorY}
+    fontSize={fontSize}
+    {color}
+    on:sync={reflow}
+  />
--- src/lib/components/Meshes.svelte
+++ src/lib/components/Meshes.svelte
@@ -2,10 +2,29 @@
 	import * as THREE from 'three'
 	import { T,useThrelte } from '@threlte/core'
 	import { useRatk } from '$lib/ratk.js'
-	import { Text } from '@threlte/extras'
+	import { Text, RoundedBoxGeometry } from '@threlte/extras'
+	import { Flex, Box } from '@threlte/flex'
+	import Label from './Label.svelte'
+	import { GLTFExporter } from "three/examples/jsm/exporters/GLTFExporter";
+	import { pointerControls, useXR } from '@threlte/xr'
 
 	const ratk = useRatk()
 	const group = new THREE.Group()
+
+	const { isPresenting } = useXR()
+	pointerControls('left')
+	pointerControls('right')
+
+	// create a function to change the color of the button
+	const changeColor = () => {
+      // if the color is #404550, change it to #96ffba
+      if (color === '#404550') {
+          color = '#96ffba'
+      } else {
+          // if the color is #96ffba, change it to #404550
+          color = '#404550'
+      }
+  }
 	
 
 	let enabled = true
@@ -57,8 +76,93 @@
 			enabled = true
 		}, 5000)
 	}
+
+	function exportGLTF() {
+    
+
+    const gltfExporter = new GLTFExporter();
+    
+    
+    gltfExporter.parse(
+	  meshes,
+      function ( result ) {
+    
+        if ( result instanceof ArrayBuffer) {
+          console.log( result );
+    
+          saveArrayBuffer( result, 'scene.glb' );
+    
+        } else {
+    
+          const output = JSON.stringify( result, null, 2 );
+          console.log( output );
+          saveString( output, 'scene.gltf' );
+    
+        }
+    
+      },
+      function ( error ) {
+    
+        console.log( 'An error happened during parsing', error );
+    
+      }
+    );
+    
+    }
+    
+    const link = document.createElement( 'a' );
+    link.style.display = 'none';
+    document.body.appendChild( link ); // Firefox workaround, see #6594
+    
+    function save( blob, filename ) {
+    
+    link.href = URL.createObjectURL( blob );
+    link.download = filename;
+    link.click();
+    
+    // URL.revokeObjectURL( url ); breaks Firefox...
+    
+    }
+    
+    function saveString( text, filename ) {
+    
+      save( new Blob( [ text ], { type: 'text/gltf' } ), filename );
+    
+    }
+    
+    
+    function saveArrayBuffer( buffer, filename ) {
+    
+    save( new Blob( [ buffer ], { type: 'application/octet-stream' } ), filename );
+    
+    }
+
+	let ref2
+	let color = '#404550'
+    let text = 'Download Model'
 </script>
 
+<Flex width={3} height={1}>
+
+    <T.Mesh
+      bind:ref2
+      position.z={-4}
+      position.y={1}
+      on:click={exportGLTF}
+      on:pointerenter={changeColor}
+    on:pointerleave={changeColor}
+    >
+      <RoundedBoxGeometry
+        args={[1, 0.3, 0.3]}
+        radius={0.1}
+      />
+      <T.MeshBasicMaterial color={color} />
+      <Label
+        z={0.2}
+        {text}
+      />
+    </T.Mesh>
+</Flex>
 
 
 {#each objects as object}
