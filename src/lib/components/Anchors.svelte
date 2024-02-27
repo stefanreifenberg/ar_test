5854f1789369185ea6b1a23cb226f7e11d14e1c4
Stefan Reifenberg <stefan.reifenberg@googlemail.com>
2024-02-27 13:34:13 +0100

    test

--- src/lib/components/Anchors.svelte
+++ src/lib/components/Anchors.svelte
@@ -30,6 +30,7 @@
     })
     const { start, stop, started, task: someTask } = useTask('some-task',(delta) => {
     // do something
+     
        console.log("initilizing persistent anchors")
         ratk.restorePersistentAnchors().then(() => {
                     // if there are more than seven anchors, remove all of them
@@ -41,38 +42,38 @@
 
     useTask((delta) => {
 
-        // if (pendingAnchorsData) {
-        //     const isPersistent = true
-        //     let anchorPosition
-        //     let anchorQuaternion
+        if (pendingAnchorsData) {
+            const isPersistent = true
+            let anchorPosition
+            let anchorQuaternion
             
-        //     const matrix = pendingAnchorsData
-        //     const position = new THREE.Vector3()
-        //     const quaternion = new THREE.Quaternion()
-        //     const scale = new THREE.Vector3()
-        //     matrix.decompose(position, quaternion, scale)
-        //     anchorPosition = position
-        //     anchorQuaternion = quaternion
+            const matrix = pendingAnchorsData
+            const position = new THREE.Vector3()
+            const quaternion = new THREE.Quaternion()
+            const scale = new THREE.Vector3()
+            matrix.decompose(position, quaternion, scale)
+            anchorPosition = position
+            anchorQuaternion = quaternion
 
-        //     const geometry = new THREE.BoxGeometry(1, 1, 1);
-        //     const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+            const geometry = new THREE.BoxGeometry(1, 1, 1);
+            const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 
-        //     // create an anchor
-        //     console.log('creating anchor')
-        //     console.log("ratk",ratk)
-        //     console.log("ratk.anchors",ratk.anchors)
+            // create an anchor
+            console.log('creating anchor')
+            console.log("ratk",ratk)
+            console.log("ratk.anchors",ratk.anchors)
 
-        //     ratk.createAnchor(anchorPosition, anchorQuaternion, isPersistent)
-        //         .then((anchor /* RATK Anchor object extends Object3D */) => {
+            ratk.createAnchor(anchorPosition, anchorQuaternion, isPersistent)
+                .then((anchor /* RATK Anchor object extends Object3D */) => {
 
-        //             console.log('anchor', anchor)
+                    console.log('anchor', anchor)
 
-        //             // Attach a new THREE.Mesh to the anchor
-        //             anchor.add(new THREE.Mesh(geometry, material));
-        //             anchors = [...ratk.anchors]
-        //         });
-        // }
-        // pendingAnchorsData = null
+                    // Attach a new THREE.Mesh to the anchor
+                    anchor.add(new THREE.Mesh(geometry, material));
+                    anchors = [...ratk.anchors]
+                });
+        }
+        pendingAnchorsData = null
   })
    
 
