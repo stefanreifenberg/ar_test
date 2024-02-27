8ead02f6aec3d12d2f2834ad7ef0da666b7af6a0
Stefan Reifenberg <stefan.reifenberg@googlemail.com>
2024-02-27 14:22:30 +0100

    test pers anchors

--- src/lib/components/Anchors.svelte
+++ src/lib/components/Anchors.svelte
@@ -32,7 +32,7 @@
         }
         
     })
-    const { start, stop, started } = useTask(,(delta) => {
+    const { start, stop, started } = useTask((delta) => {
     // do something
      
        console.log("initilizing persistent anchors")
@@ -46,38 +46,46 @@
 
     useTask((delta) => {
 
-        if (pendingAnchorsData) {
-            const isPersistent = true
-            let anchorPosition
-            let anchorQuaternion
-            
-            const matrix = pendingAnchorsData
-            const position = new THREE.Vector3()
-            const quaternion = new THREE.Quaternion()
-            const scale = new THREE.Vector3()
-            matrix.decompose(position, quaternion, scale)
-            anchorPosition = position
-            anchorQuaternion = quaternion
-
-            const geometry = new THREE.BoxGeometry(1, 1, 1);
-            const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-
-            // create an anchor
-            console.log('creating anchor')
-            console.log("ratk",ratk)
-            console.log("ratk.anchors",ratk.anchors)
-
-            ratk.createAnchor(anchorPosition, anchorQuaternion, isPersistent)
-                .then((anchor /* RATK Anchor object extends Object3D */) => {
-
-                    console.log('anchor', anchor)
-
-                    // Attach a new THREE.Mesh to the anchor
-                    anchor.add(new THREE.Mesh(geometry, material));
-                    anchors = [...ratk.anchors]
+        ratk.restorePersistentAnchors().then(() => {
+                    // if there are more than seven anchors, remove all of them
+                    ratk.anchors.forEach((anchor) => {
+                        console.log("anchor", anchor)
+                        buildAnchorMarker(anchor, true);
+                    });
                 });
-        }
-        pendingAnchorsData = null
+
+        // if (pendingAnchorsData) {
+        //     const isPersistent = true
+        //     let anchorPosition
+        //     let anchorQuaternion
+            
+        //     const matrix = pendingAnchorsData
+        //     const position = new THREE.Vector3()
+        //     const quaternion = new THREE.Quaternion()
+        //     const scale = new THREE.Vector3()
+        //     matrix.decompose(position, quaternion, scale)
+        //     anchorPosition = position
+        //     anchorQuaternion = quaternion
+
+        //     const geometry = new THREE.BoxGeometry(1, 1, 1);
+        //     const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+
+        //     // create an anchor
+        //     console.log('creating anchor')
+        //     console.log("ratk",ratk)
+        //     console.log("ratk.anchors",ratk.anchors)
+
+        //     ratk.createAnchor(anchorPosition, anchorQuaternion, false)
+        //         .then((anchor /* RATK Anchor object extends Object3D */) => {
+
+        //             console.log('anchor', anchor)
+
+        //             // Attach a new THREE.Mesh to the anchor
+        //             anchor.add(new THREE.Mesh(geometry, material));
+        //             anchors = [...ratk.anchors]
+        //         });
+        // }
+        // pendingAnchorsData = null
   })
    
 
